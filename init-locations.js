973814050302b0a0ac6aadb4ac8287d9da73ecc9
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-01-27 16:18:22 -0600

    Update init-locations.js

--- init-locations.js
+++ init-locations.js
@@ -4,16 +4,16 @@ export default [
       
     {
         Name: 'McCrimon Parkway',
-        North: 35.841638, 
+        North: 35.8416, 
         West: -78.852915,
      },
         
     
     {
         Name: 'Church street',
-        North: 35.878, 
-        West: -78.894,
-        South: 35.88, 
+        North: 35.842, 
+        West: -78.844,
+        South: 35.849, 
         East: -78.845,
     }
       
