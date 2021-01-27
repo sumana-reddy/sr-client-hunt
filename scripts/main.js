4bf07a2e499fe732b39723f6e600981963b820a8
sumanareddy <s538360@nwmissouri.edu>
2021-01-27 09:39:42 -0600

    added location

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,48 @@
 /**
+ * main.js - the logic for our app
  * Get the location
- * Uses new import / export - be sure to set type="module" in HTML
- * Can be easily added to any web page.
- * Includes GeoLocation API example.
  * @author Sumana Reddy Reddybathula
  */
-window.addEventListener('load', (main) => {
-    console.log("Loaded Page");
-  });
+
+// first imports..............
+import getlocation from './location.js';
+
+// helper functions .........
+
+
+
+// event handlers...........
+
+async function locationHandler() {
+  const locText = await getLocation();
+  document.getElementById('locationAnswer').innerHTML = locText;
+}
+
+function clearErrorText() {
+  document.getElementById('error-message').innerHTML = '';
+}
+
+
+// declare main method................
+function main() {
+  console.log('Starting main method...');
+
+  // get references to html elements
+  const locationElement = document.getElementById('location');
+  const errorElement = document.getElementById('error-message');
+
+  // init error to empty string
+  errorElement.innerHTML = '';
+
+  locationElement.addEventListener('click', locationHandler);
+  locationElement.addEventListener('touch', locationHandler);
+}
+
+// this is where it begins
+window.addEventListener('load', main);
+/*window.addEventListener('load', (main) => {
+  console.log("Loaded Page");
+});*/
+
+//window.addEventListener('load', main);
 
