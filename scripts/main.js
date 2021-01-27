0b85c874fca88aa958b1a77dfe063de1303a1eac
sumanareddy <s538360@nwmissouri.edu>
2021-01-27 09:47:45 -0600

    modified

--- scripts/main.js
+++ scripts/main.js
@@ -4,45 +4,42 @@
  * @author Sumana Reddy Reddybathula
  */
 
-// first imports..............
-import getlocation from './location.js';
+// first imports.......................
+import getLocation from './location.js';
 
-// helper functions .........
+// helper functions....................
 
 
+// event handlers......................
 
-// event handlers...........
-
+/**
+ * Wait to get location and then display it.
+ * Location should only be updated in response to a USER GESTURE
+ */
 async function locationHandler() {
-  const locText = await getLocation();
-  document.getElementById('locationAnswer').innerHTML = locText;
+    const locText = await getLocation();
+    document.getElementById('locationAnswer').innerHTML = locText;
 }
 
 function clearErrorText() {
-  document.getElementById('error-message').innerHTML = '';
+    document.getElementById('error-message').innerHTML = '';
 }
 
 
 // declare main method................
 function main() {
-  console.log('Starting main method...');
+    console.log('Starting main method...');
 
-  // get references to html elements
-  const locationElement = document.getElementById('location');
-  const errorElement = document.getElementById('error-message');
+    // get references to html elements
+    const locationElement = document.getElementById('location');
+    const errorElement = document.getElementById('error-message');
 
-  // init error to empty string
-  errorElement.innerHTML = '';
+    // init error to empty string
+    errorElement.innerHTML = '';
 
-  locationElement.addEventListener('click', locationHandler);
-  locationElement.addEventListener('touch', locationHandler);
+    locationElement.addEventListener('click', locationHandler);
+    locationElement.addEventListener('touch', locationHandler);
 }
 
 // this is where it begins
 window.addEventListener('load', main);
-/*window.addEventListener('load', (main) => {
-  console.log("Loaded Page");
-});*/
-
-//window.addEventListener('load', main);
-
