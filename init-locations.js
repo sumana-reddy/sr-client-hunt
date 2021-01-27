5355d0950829007591b1d9264a932aed71c1733f
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-01-27 10:47:39 -0600

    updated

--- init-locations.js
+++ init-locations.js
@@ -5,16 +5,16 @@ export default [
     {
         Name: '5860 Mc cRIMMON Parkway',
         North: 35.84234912578917, 
-        West: -78.84515738749143,
+        West: -78.84515738749143
       },
         
     
     {
         Name: 'North West Missouri State University',
         North: 40.351952674389615, 
-        West: -94.88241701431218,
+        West: -94.88241701431218
         
-    },
+    }
         
   
   ];
