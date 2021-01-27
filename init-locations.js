20430fedb2b9fcb593ef60f8c37d7f54423895ac
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-01-27 15:51:06 -0600

    modified

--- init-locations.js
+++ init-locations.js
@@ -6,8 +6,8 @@ export default [
         Name: '5860 Mc cRIMMON Parkway',
         North: 35.84234912578917, 
         West: -78.84515738749143,
-        South: 35.842166485859934, 
-        East: -78.845371964205,
+        South: 35.841922087067964, 
+        East: -78.8453565653591,
           Notes: 'circle',
       },
         
